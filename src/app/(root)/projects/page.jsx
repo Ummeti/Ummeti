9aabe63730bc9dec54207c6780278c912a2ae7fc
tw9str <i88b@hotmail.com>
2025-02-27 15:44:14 +0100

    adjusted cache

--- src/app/(root)/projects/page.jsx
+++ src/app/(root)/projects/page.jsx
@@ -1,7 +1,9 @@
 import ProjectSection from './ProjectSection';
+import { unstable_noStore as noStore } from 'next/cache';
 import { fetchCategories, fetchProjects } from '@/lib/fetchData';
 
 export default async function Projects() {
+  noStore();
   const projects = await fetchProjects();
   const categories = await fetchCategories();
 
